0533ad0840fee64198cea1daa896537272d47788
K-KY <kyuyoungk@naver.com>
2024-01-10 02:17:41 +0900

    feat : InitialCommit

---
+++ src/test/java/com/creavispace/project/CreavispaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creavispace.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreavispaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
