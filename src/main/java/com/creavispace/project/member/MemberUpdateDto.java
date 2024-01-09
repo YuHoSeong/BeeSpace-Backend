f4004e31f150a654d90797d4304e324eec5cb1c6
K-KY <kyuyoungk@naver.com>
2024-01-10 03:41:01 +0900

    feat : MemberUpdateDto 추가
    회원이 직접 수정할 수 있는 데이터

---
+++ src/main/java/com/creavispace/project/member/MemberUpdateDto.java
@@ -0,0 +1,9 @@
+package com.creavispace.project.member;
+
+import jakarta.persistence.Column;
+
+public class MemberUpdateDto {
+    private String memberPassword;
+    private String memberNickname;
+    private String introduce;
+}
