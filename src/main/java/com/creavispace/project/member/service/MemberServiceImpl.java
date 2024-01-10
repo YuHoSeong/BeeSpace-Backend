103ac362307b7df884d76d4219cdbb6b3a69d1df
K-KY <kyuyoungk@naver.com>
2024-01-10 13:53:14 +0900

    Feat(MemberServiceImpl) : MemberService 인터페이스로 생성, 수정, 조회 구현

---
+++ src/main/java/com/creavispace/project/member/service/MemberServiceImpl.java
@@ -0,0 +1,46 @@
+package com.creavispace.project.member.service;
+
+import com.creavispace.project.member.Member;
+import com.creavispace.project.member.MemberUpdateDto;
+import com.creavispace.project.member.repository.MemberRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository repository;
+
+    @Override
+    public Member save(Member member) {
+        System.out.println(repository.existsByMemberEmail(member.getMemberEmail()));
+        return repository.save(member);
+    }
+
+    @Override
+    public void update(Long memberId, MemberUpdateDto updateParam) {
+        Member member = repository.findById(memberId).orElseThrow();
+        member.setMemberNickname(updateParam.getMemberNickname());
+        member.setMemberPassword(updateParam.getMemberPassword());
+        repository.save(member);
+    }
+
+    @Override
+    public Optional<Member> findByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findById(Long memberId) {
+        return repository.findById(memberId);
+    }
+
+    @Override
+    public boolean emailExsists(String memberEmail) {
+        return repository.existsByMemberEmail(memberEmail);
+    }
+}
