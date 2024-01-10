5b8b6c59b582cefbb2f739fc770c05b977bd30d1
K-KY <kyuyoungk@naver.com>
2024-01-10 13:50:49 +0900

    Feat(MemberUpdateDto) : MemberUpdateDto 생성자 추가

--- src/main/java/com/creavispace/project/member/MemberUpdateDto.java
+++ src/main/java/com/creavispace/project/member/MemberUpdateDto.java
@@ -1,9 +1,17 @@
 package com.creavispace.project.member;
 
 import jakarta.persistence.Column;
+import lombok.Data;
 
+@Data
 public class MemberUpdateDto {
     private String memberPassword;
     private String memberNickname;
     private String introduce;
+
+    public MemberUpdateDto(String memberPassword, String memberNickname, String introduce) {
+        this.memberPassword = memberPassword;
+        this.memberNickname = memberNickname;
+        this.introduce = introduce;
+    }
 }
