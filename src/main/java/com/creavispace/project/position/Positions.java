a1278ae0b5d7a327fa4ccb84b078d50476f169ef
K-KY <kyuyoungk@naver.com>
2024-01-13 11:58:59 +0900

    Feat(Positions) : 옵션 추가

--- src/main/java/com/creavispace/project/position/Positions.java
+++ src/main/java/com/creavispace/project/position/Positions.java
@@ -1,9 +1,9 @@
-package com.creavispace.project.member.position;
+package com.creavispace.project.position;
 
 import java.util.List;
 
 public enum Positions {
-    Backend("백엔드"), Frontend("프론트엔드"), Design("디자이너"), PM("PM"), DevOps("데브옵스");
+    Backend("백엔드"), Frontend("프론트엔드"), FullStack("풀스택"), Design("디자이너"), PM("PM"), DevOps("데브옵스");
 
     String position;
     Positions(String position) {
