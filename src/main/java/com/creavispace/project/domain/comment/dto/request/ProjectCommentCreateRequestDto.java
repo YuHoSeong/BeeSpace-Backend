4caaf530932dc84c3e724207d3e4ef07b2cf0f4c
YuHoSeong <ghtjd026@gmail.com>
2024-01-18 15:57:52 +0900

    feat(ProjectComment): Add @NoArgsConstructor, @AllArgsConstructor to DTOs in the projectComment

--- src/main/java/com/creavispace/project/domain/comment/dto/request/ProjectCommentCreateRequestDto.java
+++ src/main/java/com/creavispace/project/domain/comment/dto/request/ProjectCommentCreateRequestDto.java
@@ -1,8 +1,12 @@
 package com.creavispace.project.domain.comment.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectCommentCreateRequestDto {
     private Long projectId;
     private String content;
