ffe3868ba7be330b59445302987abdd0f5a97a30
YuHoSeong <ghtjd026@gmail.com>
2024-02-20 15:16:25 +0900

    feat(ProjectBookmarkController) : projectBookmark swagger 정보 추가

--- src/main/java/com/creavispace/project/domain/bookmark/controller/ProjectBookmarkController.java
+++ src/main/java/com/creavispace/project/domain/bookmark/controller/ProjectBookmarkController.java
@@ -1,30 +1,32 @@
 package com.creavispace.project.domain.bookmark.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.creavispace.project.domain.bookmark.dto.response.ProjectBookmarkResponseDto;
 import com.creavispace.project.domain.bookmark.service.ProjectBookmarkService;
 import com.creavispace.project.domain.common.dto.SuccessResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/project/bookmark")
+@RequestMapping("/bookmark")
 public class ProjectBookmarkController {
     
     private final ProjectBookmarkService projectBookmarkService;
 
-    private static final String PROJECT_BOOKMARK = "/{projectId}";
+    private static final String PROJECT_BOOKMARK = "";
 
-    @GetMapping(PROJECT_BOOKMARK)
-    public ResponseEntity<SuccessResponseDto<ProjectBookmarkResponseDto>> projectBookmark(@PathVariable("projectId") Long projectId) {
+    @PostMapping(PROJECT_BOOKMARK)
+    @Operation(summary = "프로젝트 북마크 토글 기능")
+    public ResponseEntity<SuccessResponseDto<ProjectBookmarkResponseDto>> projectBookmark(@RequestParam("projectId") Long projectId) {
         return ResponseEntity.ok().body(projectBookmarkService.projectBookmark(projectId));
     }
     
