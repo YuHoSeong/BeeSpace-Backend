756136ac8d94b66c1015cd59cccae26ce505b7a1
K-KY <kyuyoungk@naver.com>
2024-02-20 23:28:01 +0900

    Feat(MyPage) : 마이페이지 수정 기능 추가

---
+++ src/main/java/com/creavispace/project/domain/mypage/dto/request/MyPageModifyRequestDto.java
@@ -0,0 +1,11 @@
+package com.creavispace.project.domain.mypage.dto.request;
+
+public class MyPageModifyRequestDto {
+
+    //jwt 인증
+    private String nickName;
+    private String introduce;
+    private String position;
+    private String career;
+    private String interestedStack;
+}
