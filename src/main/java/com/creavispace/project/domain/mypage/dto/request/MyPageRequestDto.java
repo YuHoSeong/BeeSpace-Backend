f78eb7944a48f4fef5cb2e965c857f32dedafba5
K-KY <kyuyoungk@naver.com>
2024-02-20 23:17:10 +0900

    Feat(MyPage) : 마이페이지 기능 추가

---
+++ src/main/java/com/creavispace/project/domain/mypage/dto/request/MyPageRequestDto.java
@@ -0,0 +1,9 @@
+package com.creavispace.project.domain.mypage.dto.request;
+
+public class MyPageRequestDto {
+    //jwt 인증
+    private String loginId;
+    private String memberEmail;
+    private String memberName;
+
+}
