9fec96056981646d9dded24c6dae803361f904b6
YuHoSeong <ghtjd026@gmail.com>
2024-02-20 15:21:20 +0900

    feat(ProjectComment) : 프로젝트 댓글 스웨거 정보 추가 및 댓글 조회 기능 추가
    프로젝트 댓글 수정 url 변경: Pathvariable projectCommentId 추가
    프로젝트 댓글 조회 기능 추가 : Controller, Dto, Service 추가
    
    #68, #29, #30, #31

---
+++ src/main/java/com/creavispace/project/domain/comment/dto/response/ProjectCommentReadResponseDto.java
@@ -0,0 +1,21 @@
+package com.creavispace.project.domain.comment.dto.response;
+
+import org.joda.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectCommentReadResponseDto {
+    private Long id;
+    private Long memberId;
+    private String memberNickName;
+    private String memberProfileUrl;
+    private LocalDateTime modifiedDate;
+    private String content;
+}
