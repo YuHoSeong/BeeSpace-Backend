3c7c40fcb6371dc0aa873a8053255114b068c09a
YuHoSeong <ghtjd026@gmail.com>
2024-02-20 15:32:32 +0900

    feat(ProjectLike) : 프로젝트 좋아요 스웨거 정보 추가 fix : url 변경
    #32

--- src/main/java/com/creavispace/project/domain/like/controller/ProjectLikeController.java
+++ src/main/java/com/creavispace/project/domain/like/controller/ProjectLikeController.java
@@ -1,29 +1,31 @@
 package com.creavispace.project.domain.like.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.creavispace.project.domain.common.dto.SuccessResponseDto;
 import com.creavispace.project.domain.like.dto.response.ProjectLikeResponseDto;
 import com.creavispace.project.domain.like.service.ProjectLikeService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/project/like")
+@RequestMapping("/like")
 public class ProjectLikeController {
     
     private final ProjectLikeService projectLikeService;
 
-    private static final String PROJECT_LIKE = "/{projectId}";
+    private static final String PROJECT_LIKE = "";
 
     @GetMapping(PROJECT_LIKE)
-    public ResponseEntity<SuccessResponseDto<ProjectLikeResponseDto>> projectLike(@PathVariable("projectId") Long projectId) {
+    @Operation(summary = "프로젝트 좋아요 토글 기능")
+    public ResponseEntity<SuccessResponseDto<ProjectLikeResponseDto>> projectLike(@RequestParam("projectId") Long projectId) {
         return ResponseEntity.ok().body(projectLikeService.projectLike(projectId));
     }
     
