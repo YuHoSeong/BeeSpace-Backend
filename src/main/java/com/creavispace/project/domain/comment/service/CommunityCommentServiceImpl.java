845b988acd3467f9fcdd77d0daddd678dcc98f72
YuHoSeong <ghtjd026@gmail.com>
2024-02-21 13:55:46 +0900

    feat(readCommunityCommentList) : 커뮤니티 댓글 리스트 조회 API 명세서 작업 추가

---
+++ src/main/java/com/creavispace/project/domain/comment/service/CommunityCommentServiceImpl.java
@@ -0,0 +1,19 @@
+package com.creavispace.project.domain.comment.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.creavispace.project.domain.comment.dto.response.CommentResponseDto;
+import com.creavispace.project.domain.common.dto.SuccessResponseDto;
+
+@Service
+public class CommunityCommentServiceImpl implements CommunityCommentService {
+
+    @Override
+    public SuccessResponseDto<List<CommentResponseDto>> readCommunityCommentList(Long communityId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readCommunityCommentList'");
+    }
+    
+}
