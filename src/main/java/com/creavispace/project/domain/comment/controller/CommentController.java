38c8d49ed9a09a1730d898498e9065683ec49154
YuHoSeong <ghtjd026@gmail.com>
2024-02-21 13:58:17 +0900

    feat(createCommunityComment) : 커뮤니티 댓글 등록 API 명세서 작업 추가

--- src/main/java/com/creavispace/project/domain/comment/controller/CommentController.java
+++ src/main/java/com/creavispace/project/domain/comment/controller/CommentController.java
@@ -45,6 +45,7 @@ public class CommentController {
     private static final String MODIFY_RECRUIT_COMMENT = "/recruit/{recruitCommentId}";
     private static final String DELETE_RECRUIT_COMMENT = "/recruit/{recruitCommentId}";
     private static final String READ_COMMUNITY_COMMENT_LIST = "/community";
+    private static final String CREATE_COMMUNITY_COMMENT = "/community";
 
     @GetMapping(READ_PROJECT_COMMENT_LIST)
     @Operation(summary = "프로젝트 댓글 리스트 조회")
@@ -100,6 +101,14 @@ public class CommentController {
     @Operation(summary = "커뮤니티 댓글 리스트 조회")
     public ResponseEntity<SuccessResponseDto<List<CommentResponseDto>>> readCommunityCommentList(@RequestParam("communityId") Long communityId){
         return ResponseEntity.ok().body(communityCommentService.readCommunityCommentList(communityId));
+    }
+
+    @PostMapping(CREATE_COMMUNITY_COMMENT)
+    @Operation(summary = "커뮤니티 댓글 등록")
+    public ResponseEntity<SuccessResponseDto<CommentResponseDto>> createCommunityComment(
+        @RequestParam("communityId") Long communityId,
+        @RequestBody CommentRequestDto requestBody) {
+        return ResponseEntity.ok().body(communityCommentService.createCommunityComment(communityId, requestBody));
     }
 
 }
--- src/main/java/com/creavispace/project/domain/comment/service/CommunityCommentServiceImpl.java
+++ src/main/java/com/creavispace/project/domain/comment/service/CommunityCommentServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.creavispace.project.domain.comment.dto.request.CommentRequestDto;
 import com.creavispace.project.domain.comment.dto.response.CommentResponseDto;
 import com.creavispace.project.domain.common.dto.SuccessResponseDto;
 
@@ -14,6 +15,12 @@ public class CommunityCommentServiceImpl implements CommunityCommentService {
     public SuccessResponseDto<List<CommentResponseDto>> readCommunityCommentList(Long communityId) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'readCommunityCommentList'");
+    }
+
+    @Override
+    public SuccessResponseDto<CommentResponseDto> createCommunityComment(Long communityId, CommentRequestDto dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCommunityComment'");
     }
     
 }
