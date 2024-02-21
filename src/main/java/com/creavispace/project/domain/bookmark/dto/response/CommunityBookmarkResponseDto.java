bfcfe101c12a87e7e49d9363be989e92ed308496
YuHoSeong <ghtjd026@gmail.com>
2024-02-21 10:19:02 +0900

    feat(communityBookmark) : 커뮤니티 게시글 북마크 토글 기능 API명세서 추가

---
+++ src/main/java/com/creavispace/project/domain/bookmark/dto/response/CommunityBookmarkResponseDto.java
@@ -0,0 +1,14 @@
+package com.creavispace.project.domain.bookmark.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommunityBookmarkResponseDto {
+    private boolean bookmarkStatus;
+}
---
+++ src/main/java/com/creavispace/project/domain/bookmark/service/CommunityBookmarkServiceImpl.java
@@ -0,0 +1,17 @@
+package com.creavispace.project.domain.bookmark.service;
+
+import org.springframework.stereotype.Service;
+
+import com.creavispace.project.domain.bookmark.dto.response.CommunityBookmarkResponseDto;
+import com.creavispace.project.domain.common.dto.SuccessResponseDto;
+
+@Service
+public class CommunityBookmarkServiceImpl implements CommunityBookmarkService{
+
+    @Override
+    public SuccessResponseDto<CommunityBookmarkResponseDto> communityBookmark(Long communityId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'communityBookmark'");
+    }
+    
+}
