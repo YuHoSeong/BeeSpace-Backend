e7ed014d5091ad68d9c0414dec40a93025ab8991
YuHoSeong <ghtjd026@gmail.com>
2024-02-21 10:43:54 +0900

    rename : ProjectLikeController -> LikeController

--- src/main/java/com/creavispace/project/domain/like/controller/LikeController.java
+++ src/main/java/com/creavispace/project/domain/like/controller/LikeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/like")
-public class ProjectLikeController {
+public class LikeController {
     
     private final ProjectLikeService projectLikeService;
 
