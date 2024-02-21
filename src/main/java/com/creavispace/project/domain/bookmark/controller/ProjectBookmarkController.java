e1f5402d9e10d93ff87ed4b4245c69e87193fb71
YuHoSeong <ghtjd026@gmail.com>
2024-02-21 09:46:46 +0900

    fix : 프로젝트 북마크 토글 기능의 url 변경
    /bookmark -> /bookmark/project/{projectId}

--- src/main/java/com/creavispace/project/domain/bookmark/controller/ProjectBookmarkController.java
+++ src/main/java/com/creavispace/project/domain/bookmark/controller/ProjectBookmarkController.java
@@ -1,7 +1,6 @@
 package com.creavispace.project.domain.bookmark.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.creavispace.project.domain.bookmark.dto.response.ProjectBookmarkResponseDto;
@@ -12,6 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -22,11 +22,11 @@ public class ProjectBookmarkController {
     
     private final ProjectBookmarkService projectBookmarkService;
 
-    private static final String PROJECT_BOOKMARK = "";
+    private static final String TOGGLE_PROJECT_BOOKMARK = "/project/{projectId}";
 
-    @PostMapping(PROJECT_BOOKMARK)
+    @PostMapping(TOGGLE_PROJECT_BOOKMARK)
     @Operation(summary = "프로젝트 북마크 토글 기능")
-    public ResponseEntity<SuccessResponseDto<ProjectBookmarkResponseDto>> projectBookmark(@RequestParam("projectId") Long projectId) {
+    public ResponseEntity<SuccessResponseDto<ProjectBookmarkResponseDto>> projectBookmark(@PathVariable("projectId") Long projectId) {
         return ResponseEntity.ok().body(projectBookmarkService.projectBookmark(projectId));
     }
     
