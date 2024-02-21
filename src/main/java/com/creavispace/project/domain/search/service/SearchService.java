9041d688d29981ef00db58d5961fdf36e349aac1
YuHoSeong <ghtjd026@gmail.com>
2024-02-21 09:32:48 +0900

    feat(readSearchProject) : 통합검색 프로젝트 게시글 카드 정보 조회 API명세서 추가

--- src/main/java/com/creavispace/project/domain/search/service/SearchService.java
+++ src/main/java/com/creavispace/project/domain/search/service/SearchService.java
@@ -3,8 +3,10 @@ package com.creavispace.project.domain.search.service;
 import java.util.List;
 
 import com.creavispace.project.domain.common.dto.SuccessResponseDto;
+import com.creavispace.project.domain.project.dto.response.ProjectListReadResponseDto;
 import com.creavispace.project.domain.search.dto.response.SearchListReadResponseDto;
 
 public interface SearchService {
     public SuccessResponseDto<List<SearchListReadResponseDto>> readSearchList(Integer size, Integer page, String text, String type);
+    public SuccessResponseDto<ProjectListReadResponseDto> readSearchProject(Long projectId);
 }
