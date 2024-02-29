eea0dc319f08cac4131c46262c1a5da3ecc869f1
YuHoSeong <ghtjd026@gmail.com>
2024-02-29 14:41:50 +0900

    feat(communityLike) : 커뮤니티 좋아요 토글 ServiceImpl 구현
    #41

---
+++ src/main/java/com/creavispace/project/domain/like/repository/CommunityLikeRepository.java
@@ -0,0 +1,11 @@
+package com.creavispace.project.domain.like.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.creavispace.project.domain.like.entity.CommunityLike;
+
+@Repository
+public interface CommunityLikeRepository extends JpaRepository<CommunityLike, Long> {
+    public CommunityLike findByCommunityIdAndMemberId(Long communityId, Long memberId);
+}
