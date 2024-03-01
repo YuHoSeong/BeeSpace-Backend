7a7ad9675a4a9f089745f4f65eb97005944c5550
YuHoSeong <ghtjd026@gmail.com>
2024-03-01 15:25:37 +0900

    feat(readSearchList) : 통합검색 리스트 조회 ServiceImpl 구현 fix(communityRepository) : 해시태그 관련 메서드 에러로 Query추가

--- src/main/java/com/creavispace/project/domain/community/repository/CommunityRepository.java
+++ src/main/java/com/creavispace/project/domain/community/repository/CommunityRepository.java
@@ -5,15 +5,34 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.creavispace.project.domain.community.entity.Community;
+import com.creavispace.project.domain.search.entity.SearchResult;
 
 @Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
     public Optional<Community> findByIdAndStatusTrue(Long communityId);
+    
+    @Query(value = "SELECT * FROM community c JOIN community_hash_tag ch ON c.id = ch.community_id WHERE ch.hash_tag_id = :hashTagId AND category = :category AND status = true ORDER BY created_date DESC", nativeQuery = true)
     public Page<Community> findAllByStatusTrueAndCategoryAndHashTagId(String category, Long hashTagId, Pageable pageable);
+
+    @Query(value = "SELECT * FROM community c JOIN community_hash_tag ch ON c.id = ch.community_id WHERE ch.hash_tag_id = :hashTagId AND status = true ORDER BY created_date DESC", nativeQuery = true)
     public Page<Community> findAllByStatusTrueAndHashTagId(Long hashTagId, Pageable pageable);
+
     public Page<Community> findAllByStatusTrueAndCategory(String category, Pageable pageable);
+
     public Page<Community> findAllByStatusTrue(Pageable pageable);
+
+    @Query(value = "SELECT 'community' AS postType, c.id AS postId FROM Community c WHERE (c.content LIKE %:text% OR c.title LIKE %:text%) AND c.status = true ORDER BY created_date DESC", nativeQuery = true)
+    Page<SearchResult> findCommunitySearchData(String text, Pageable pageable);
+
+    @Query(value = "SELECT 'project' AS postType, p.id AS postId FROM Project p WHERE (p.content LIKE %:text% OR p.title LIKE %:text%) AND p.status = true " +
+           "UNION " +
+           "SELECT 'recruit' AS postType, r.id AS postId FROM Recruit r WHERE (r.content LIKE %:text% OR r.title LIKE %:text%) AND r.status = true " +
+           "UNION " +
+           "SELECT 'community' AS postType, c.id AS postId FROM Community c WHERE (c.content LIKE %:text% OR c.title LIKE %:text%) AND c.status = true " +
+           "ORDER BY created_date DESC ",nativeQuery = true)
+    Page<SearchResult> findIntegratedSearchData(String text, Pageable pageable);
 }
--- src/main/java/com/creavispace/project/global/exception/GlobalErrorCode.java
+++ src/main/java/com/creavispace/project/global/exception/GlobalErrorCode.java
@@ -14,10 +14,12 @@ public enum GlobalErrorCode {
     PROJECT_COMMENT_NOT_FOUND(400, "해당 댓글이 존재하지 않습니다."),
     S3_SERVER_NOT_FOUND(500, "이미지 저장에 실패했습니다."),
     RECRUIT_NOT_FOUND(400,"해당 모집 게시글이 존재하지 않습니다."),
-    NOT_RECRUIT_CONTENT(400,"모집 게시글이 없습니다."),
+    NOT_RECRUIT_CONTENT(400,"모집 게시글 조회 결과가 없습니다."),
     COMMUNITY_NOT_FOUND(400, "해당 커뮤니티 게시글이 존재하지 않습니다."),
-    NOT_COMMUNITY_CONTENT(400, "커뮤니티 게시글이 없습니다."),
-    LIKE_COUNT_NOT_FOUND(400, "좋아요 수 조회에 실패하였습니다.");
+    NOT_COMMUNITY_CONTENT(400, "커뮤니티 게시글 조회 결과가 없습니다."),
+    LIKE_COUNT_NOT_FOUND(400, "좋아요 수 조회에 실패하였습니다."),
+    TYPE_NOT_FOUND(400,"게시글 타입을 찾을수 없습니다."),
+    NOT_SEARCH_CONTENT(400, "검색 결과가 없습니다.");
 
     private Integer code;
     private String message;
