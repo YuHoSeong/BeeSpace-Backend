3938651d64515085ff8ebcd6968128b850873f7f
YuHoSeong <ghtjd026@gmail.com>
2024-03-02 20:06:23 +0900

    fix : communityHashTagRepository, RecruitPositionRepository의 Query에 nativeQuery속성 추가

--- src/main/java/com/creavispace/project/domain/recruit/repository/RecruitPositionRepository.java
+++ src/main/java/com/creavispace/project/domain/recruit/repository/RecruitPositionRepository.java
@@ -12,6 +12,6 @@ import com.creavispace.project.domain.recruit.entity.RecruitPosition;
 public interface RecruitPositionRepository extends JpaRepository<RecruitPosition, Long> {
     public List<RecruitPosition> findByRecruitId(Long recruitId);
     public void deleteByRecruitId(Long recruitId);
-    @Query(value = "SELECT SUM(now) FROM RecruitPosition WHERE recruit_id = :recruitId")
+    @Query(value = "SELECT SUM(now) FROM RecruitPosition WHERE recruit_id = :recruitId", nativeQuery = true)
     public Integer countByNow(Long recruitId);
 }
