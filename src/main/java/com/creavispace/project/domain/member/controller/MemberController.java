421e12e2da9e10bd2a0c997be61b78a48ecbaf2c
YuHoSeong <ghtjd026@gmail.com>
2024-03-04 16:24:06 +0900

    fix : 컴파일에러 import 추가/제거

--- src/main/java/com/creavispace/project/domain/member/controller/MemberController.java
+++ src/main/java/com/creavispace/project/domain/member/controller/MemberController.java
@@ -1,7 +1,5 @@
 package com.creavispace.project.domain.member.controller;
 
-import com.creavispace.project.config.auth.dto.SessionMember;
-import com.creavispace.project.domain.member.dto.request.MemberInfoRequestDto;
 import com.creavispace.project.domain.member.dto.request.MemberSaveRequestDto;
 import com.creavispace.project.domain.member.dto.response.MemberResponseDto;
 import com.creavispace.project.domain.member.entity.Member;
