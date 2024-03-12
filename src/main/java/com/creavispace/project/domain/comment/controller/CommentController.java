636fcf23e6fd49187fc352d83ec9b4f5d1786b0a
YuHoSeong <ghtjd026@gmail.com>
2024-03-12 13:37:19 +0900

    remove(comment) : 프로젝트/모집/커뮤니티로 각각의 기능을 삭제(통합으로 구현함)

--- src/main/java/com/creavispace/project/domain/comment/controller/CommentController.java
+++ src/main/java/com/creavispace/project/domain/comment/controller/CommentController.java
@@ -10,9 +10,6 @@ import com.creavispace.project.domain.comment.dto.request.CommentRequestDto;
 import com.creavispace.project.domain.comment.dto.response.CommentResponseDto;
 import com.creavispace.project.domain.comment.dto.response.CommentDeleteResponseDto;
 import com.creavispace.project.domain.comment.service.CommentService;
-import com.creavispace.project.domain.comment.service.CommunityCommentService;
-import com.creavispace.project.domain.comment.service.ProjectCommentService;
-import com.creavispace.project.domain.comment.service.RecruitCommentService;
 import com.creavispace.project.domain.common.dto.SuccessResponseDto;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,27 +29,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/comment")
 public class CommentController {
     
-    private final ProjectCommentService projectCommentService;
-    private final RecruitCommentService recruitCommentService;
-    private final CommunityCommentService communityCommentService;
     private final CommentService commentService;
 
     private static final String READ_COMMENT_LIST = "";
     private static final String CREATE_COMMENT = "";
     private static final String MODIFY_COMMENT = "/{commentId}";
     private static final String DELETE_COMMENT = "/{commentId}";
-    private static final String READ_PROJECT_COMMENT_LIST = "/project";
-    private static final String CREATE_PROJECT_COMMENT = "/project";
-    private static final String MODIFY_PROJECT_COMMENT = "/project/{projectCommentId}";
-    private static final String DELETE_PROJECT_COMMENT = "/project/{projectCommentId}";
-    private static final String READ_RECRUIT_COMMENT_LIST = "/recruit";
-    private static final String CREATE_RECRUIT_COMMENT = "/recruit";
-    private static final String MODIFY_RECRUIT_COMMENT = "/recruit/{recruitCommentId}";
-    private static final String DELETE_RECRUIT_COMMENT = "/recruit/{recruitCommentId}";
-    private static final String READ_COMMUNITY_COMMENT_LIST = "/community";
-    private static final String CREATE_COMMUNITY_COMMENT = "/community";
-    private static final String MODIFY_COMMUNITY_COMMENT = "/community/{communityCommentId}";
-    private static final String DELETE_COMMUNITY_COMMENT = "/community/{communityCommentId}";
 
     @GetMapping(READ_COMMENT_LIST)
     @Operation(summary = "댓글 리스트 조회")
@@ -92,84 +74,6 @@ public class CommentController {
         @RequestParam("type") String type
     ){
         return ResponseEntity.ok().body(commentService.deleteComment(memberId, commentId, type));
-    }
-
-    @GetMapping(READ_PROJECT_COMMENT_LIST)
-    @Operation(summary = "프로젝트 댓글 리스트 조회")
-    public ResponseEntity<SuccessResponseDto<List<CommentResponseDto>>> readProjectCommentList(@RequestParam("projectId") Long projectId){
-        return ResponseEntity.ok().body(projectCommentService.readProjectCommentList(projectId));
-    }
-
-    @PostMapping(CREATE_PROJECT_COMMENT)
-    @Operation(summary = "프로젝트 댓글 등록")
-    public ResponseEntity<SuccessResponseDto<CommentResponseDto>> createProjectComment(
-        @RequestParam("projectId") Long projectId,
-        @RequestBody CommentRequestDto requestBody) {
-        return ResponseEntity.ok().body(projectCommentService.createProjectComment(projectId, requestBody));
-    }
-
-    @PutMapping(MODIFY_PROJECT_COMMENT)
-    @Operation(summary = "프로젝트 댓글 수정")
-    public ResponseEntity<SuccessResponseDto<CommentResponseDto>> modifyProjectComment(@PathVariable("projectCommentId") Long projectCommentId, @RequestBody CommentRequestDto requestBody) {
-        return ResponseEntity.ok().body(projectCommentService.modifyProjectComment(projectCommentId, requestBody));
-    }
-
-    @DeleteMapping(DELETE_PROJECT_COMMENT)
-    @Operation(summary = "프로젝트 댓글 삭제")
-    public ResponseEntity<SuccessResponseDto<CommentDeleteResponseDto>> deleteProjectComment(@PathVariable("projectCommentId") Long projectCommentId){
-        return ResponseEntity.ok().body(projectCommentService.deleteProjectComment(projectCommentId));
-    }
-
-    @GetMapping(READ_RECRUIT_COMMENT_LIST)
-    @Operation(summary = "모집 댓글 리스트 조회")
-    public ResponseEntity<SuccessResponseDto<List<CommentResponseDto>>> readRecruitCommentList(@RequestParam("recruitId") Long recruitId){
-        return ResponseEntity.ok().body(recruitCommentService.readRecruitCommentList(recruitId));
-    }
-    
-    @PostMapping(CREATE_RECRUIT_COMMENT)
-    @Operation(summary = "모집 댓글 등록")
-    public ResponseEntity<SuccessResponseDto<CommentResponseDto>> createRecruitComment(
-        @RequestParam("recruitId") Long recruitId,
-        @RequestBody CommentRequestDto requestBody) {
-        return ResponseEntity.ok().body(recruitCommentService.createRecruitComment(recruitId, requestBody));
-    }
-
-    @PutMapping(MODIFY_RECRUIT_COMMENT)
-    @Operation(summary = "모집 댓글 수정")
-    public ResponseEntity<SuccessResponseDto<CommentResponseDto>> modifyRecruitComment(@PathVariable("recruitCommentId") Long recruitCommentId, @RequestBody CommentRequestDto requestBody) {
-        return ResponseEntity.ok().body(recruitCommentService.modifyRecruitComment(recruitCommentId, requestBody));
-    }
-
-    @DeleteMapping(DELETE_RECRUIT_COMMENT)
-    @Operation(summary = "모집 댓글 삭제")
-    public ResponseEntity<SuccessResponseDto<CommentDeleteResponseDto>> deleteRecruitComment(@PathVariable("recruitCommentId") Long recruitCommentId){
-        return ResponseEntity.ok().body(recruitCommentService.deleteRecruitComment(recruitCommentId));
-    }
-
-    @GetMapping(READ_COMMUNITY_COMMENT_LIST)
-    @Operation(summary = "커뮤니티 댓글 리스트 조회")
-    public ResponseEntity<SuccessResponseDto<List<CommentResponseDto>>> readCommunityCommentList(@RequestParam("communityId") Long communityId){
-        return ResponseEntity.ok().body(communityCommentService.readCommunityCommentList(communityId));
-    }
-
-    @PostMapping(CREATE_COMMUNITY_COMMENT)
-    @Operation(summary = "커뮤니티 댓글 등록")
-    public ResponseEntity<SuccessResponseDto<CommentResponseDto>> createCommunityComment(
-        @RequestParam("communityId") Long communityId,
-        @RequestBody CommentRequestDto requestBody) {
-        return ResponseEntity.ok().body(communityCommentService.createCommunityComment(communityId, requestBody));
-    }
-
-    @PutMapping(MODIFY_COMMUNITY_COMMENT)
-    @Operation(summary = "커뮤니티 댓글 수정")
-    public ResponseEntity<SuccessResponseDto<CommentResponseDto>> modifyCommunityComment(@PathVariable("communityCommentId") Long communityCommentId, @RequestBody CommentRequestDto requestBody) {
-        return ResponseEntity.ok().body(communityCommentService.modifyCommunityComment(communityCommentId, requestBody));
-    }
-
-    @DeleteMapping(DELETE_COMMUNITY_COMMENT)
-    @Operation(summary = "커뮤니티 댓글 삭제")
-    public ResponseEntity<SuccessResponseDto<CommentDeleteResponseDto>> deleteCommunityComment(@PathVariable("communityCommentId") Long communityCommentId){
-        return ResponseEntity.ok().body(communityCommentService.deleteCommunityComment(communityCommentId));
     }
 
 }
