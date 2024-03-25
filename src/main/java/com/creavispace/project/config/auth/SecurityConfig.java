507a25ed2f32f541a8e766c389de9726e2e97d09
K-KY <kyuyoungk@naver.com>
2024-03-25 10:35:52 +0900

    Refacrot(Oauth) : cors 설정 수정

--- src/main/java/com/creavispace/project/config/auth/SecurityConfig.java
+++ src/main/java/com/creavispace/project/config/auth/SecurityConfig.java
@@ -95,7 +95,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 Arrays.asList("https://creavispace.vercel.app",
                         "localhost:8080/oauth2/authorization/naver",
-                        "https://port-0-creavispace-backend-am952nlsse11uk.sel5.cloudtype.app/login/oauth2/code/naver"));
+                        "https://port-0-creavispace-backend-am952nlsse11uk.sel5.cloudtype.app/login",
+                        "localhost:8080/oauth2/authorization/login"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
 
         configuration.setAllowCredentials(true);
