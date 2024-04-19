2f3ba16e07d9cd12d3efbff222d893a03ea314cb
K-KY <kyuyoungk@naver.com>
2024-04-19 19:00:42 +0900

    Refactor(RecruitService) : 사용하지 않는 메서드 삭제

--- src/main/java/com/creavispace/project/domain/recruit/service/RecruitService.java
+++ src/main/java/com/creavispace/project/domain/recruit/service/RecruitService.java
@@ -2,7 +2,6 @@ package com.creavispace.project.domain.recruit.service;
 
 import com.creavispace.project.domain.common.dto.response.SuccessResponseDto;
 import com.creavispace.project.domain.common.dto.type.RecruitCategory;
-import com.creavispace.project.domain.member.entity.Member;
 import com.creavispace.project.domain.recruit.dto.request.RecruitRequestDto;
 import com.creavispace.project.domain.recruit.dto.response.*;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,7 +17,6 @@ public interface RecruitService {
     public SuccessResponseDto<List<DeadLineRecruitListReadResponseDto>> readDeadlineRecruitList();
 
     //ky
-    SuccessResponseDto<List<RecruitListReadResponseDto>> readMyRecruitList(Member member, Integer size, Integer page, RecruitCategory category, String sortType);
     SuccessResponseDto<List<RecruitListReadResponseDto>> readMyRecruitList(String memberId, Integer size, Integer page, String sortType);
     SuccessResponseDto<List<RecruitListReadResponseDto>> readRecruitListForAdmin(Integer size, Integer page, String status, String sortType);
 }
