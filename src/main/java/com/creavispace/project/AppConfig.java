fc40d133b21783a3c5145ce757d3dce089dd0330
YuHoSeong <ghtjd026@gmail.com>
2024-05-21 22:01:44 +0900

    Remove : 사용되지 않는 클래스 삭제

--- src/main/java/com/creavispace/project/AppConfig.java
+++
@@ -1,4 +0,0 @@
-package com.creavispace.project;
-
-public class AppConfig {
-}
--- src/main/java/com/creavispace/project/config/auth/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.creavispace.project.config.auth;
-
-import com.creavispace.project.config.auth.dto.LoginResponseDto;
-import com.creavispace.project.domain.common.dto.response.SuccessResponseDto;
-
-public interface LoginService {
-    public SuccessResponseDto<LoginResponseDto> naverLogin(String code, String state);
-}
