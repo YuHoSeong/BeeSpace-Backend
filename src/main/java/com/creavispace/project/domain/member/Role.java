30dcc349a0f6cd2d620d48f0ee2fca793d83a211
K-KY <kyuyoungk@naver.com>
2024-05-25 23:43:24 +0900

    Refactor(Role) : 방문자 권한을 탈퇴한 사용자로 변경

--- src/main/java/com/creavispace/project/domain/member/Role.java
+++ src/main/java/com/creavispace/project/domain/member/Role.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_GUEST", "방문자"),
+    EX_MEMBER("ROLE_EX_MEMBER", "탈톼한 사용자"),
     MEMBER("ROLE_MEMBER", "일반 사용자"),
     ADMIN("ROLE_ADMIN", "관리자 사용자");
 
